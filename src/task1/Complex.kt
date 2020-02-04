177761c8a4209e7dffbb29087045f94d93c1e6a1
vitekkor <viktor.korotkikh@gmail.com>
2020-02-04 19:30:58 +0300

    Complex + test

--- src/task1/Complex.kt
+++ src/task1/Complex.kt
@@ -8,51 +8,72 @@ package task1
  *
  * Аргументы конструктора -- вещественная и мнимая часть числа.
  */
+private val reg = Regex("""[+-]?\d*[.]?\d""")
 
 class Complex(val re: Double, val im: Double) {
 
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(
+        reg.findAll(s).elementAt(0).value.toDouble(),
+        reg.findAll(s).elementAt(1).value.toDouble()
+    )
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(re * other.re - im * other.im, re * other.im + im * other.re)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex = Complex(
+        (re * other.re + im * other.im) / (sqr(other.re) + sqr(other.im)),
+        (im * other.re - re * other.im) / (sqr(other.re) + sqr(other.im))
+    )
+
+    private fun sqr(num: Double): Double = num * num
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex && re == other.re && im == other.im
+
+    override fun hashCode(): Int {
+        var result = 1
+        result = 31 * result + re.toBits().toInt()
+        result = 31 * result + im.toBits().toInt()
+        return result
+    }
+
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = "$re" + when {
+        im > 0 -> "+$im"
+        im == 0.0 -> ""
+        else -> "$im"
+    } + "i"
 }
--- tests/task1/ComplexTest.kt
+++ tests/task1/ComplexTest.kt
@@ -14,26 +14,45 @@ internal class ComplexTest {
     @Test
     fun plus() {
         assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("9+14i"), Complex("2+5i") + Complex("7+9i"), 1e-10)
+        assertApproxEquals(Complex("9+4i"), Complex("2-5i") + Complex("7+9i"), 1e-10)
+        assertApproxEquals(Complex("0+0i"), Complex("0+0i") + Complex("0+0i"), 1e-10)
+        assertApproxEquals(Complex("-2+5i"), Complex("0+0i") + Complex("-2+5i"), 1e-10)
+        assertApproxEquals(Complex("0+0i"), Complex("-2+5i") + Complex("2-5i"), 1e-10)
+        assertApproxEquals(Complex("55-2i"), Complex("55+2i") + Complex("0-4i"), 1e-10)
+        assertApproxEquals(Complex("55+2i"), Complex("55+2i") + Complex("0+0i"), 1e-10)
     }
 
     @Test
     operator fun unaryMinus() {
-        assertApproxEquals(Complex(1.0, -2.0), -Complex(2.0, -1.0), 1e-10)
+        assertApproxEquals(Complex(-2.0, 1.0), -Complex(2.0, -1.0), 1e-10)
+        assertApproxEquals(Complex(-5.0, 1.0), -Complex(5.0, -1.0), 1e-10)
+        assertApproxEquals(Complex(0.0, 0.0), -Complex(0.0, 0.0), 1e-10)
+        assertApproxEquals(Complex(2.0, 1.0), -Complex(-2.0, -1.0), 1e-10)
+        assertApproxEquals(Complex(-2.0, -1.0), -Complex(2.0, 1.0), 1e-10)
+        assertApproxEquals(Complex("-1-2i"), -Complex("1+2i"), 1e-10)
+        assertApproxEquals(Complex("-55-2i"), -Complex("55+2i"), 1e-10)
+        assertApproxEquals(Complex("0+0i"), -Complex("0+0i"), 1e-10)
     }
 
     @Test
     fun minus() {
-        assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("1+2i"), Complex("4-2i") - Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("3-4i"), Complex("4-2i") - Complex("1+2i"), 1e-10)
+        assertApproxEquals(Complex("55+2i"), Complex("55-2i") - Complex("0-4i"), 1e-10)
+        assertApproxEquals(Complex("55+2i"), Complex("55+2i") - Complex("0+0i"), 1e-10)
     }
 
     @Test
     fun times() {
-        assertApproxEquals(Complex("11-8i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("11-8i"), Complex("2.6+0.8i") * Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("11+2i"), Complex("1+2i") * Complex("3-4i"), 1e-10)
     }
 
     @Test
     fun div() {
-        assertApproxEquals(Complex("1+2i"), Complex("11-8i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("2.6+0.8i"), Complex("11-8i") / Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("1+2i"), Complex("11+2i") / Complex("3-4i"), 1e-10)
     }
 
     @Test
